32ee09c2f5910b4bea32813c69de03b6a4c05e12
melissa-24 <melissalongenberger24@gmail.com>
2020-04-08 22:15:12 -0400

    changed scoreboard to baseball and added extra functions

--- src/App.js
+++ src/App.js
@@ -1,39 +1,113 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+
+  const homeInitialState = 0;
+  const [home, setScoreHome] = useState(homeInitialState);
+  const awayInitialState = 0;
+  const [away, setScoreAway] = useState(awayInitialState);
+  const inningInitialState = 1;
+  const [inning, setInning] = useState(inningInitialState);
+  const ballsInitialState = "";
+  const [balls, setBalls] = useState(ballsInitialState);
+  const strikesInitialState = "";
+  const [strikes, setStrikes] = useState(strikesInitialState);
+  const outsInitialState = "";
+  const [outs, setOuts] = useState(outsInitialState);
+
+  const addYankeesRun = () => {
+    setScoreHome(home + 1);
+    setBalls(ballsInitialState);
+    setStrikes(strikesInitialState);
+  };
+  const addGuestRun = () => {
+    setScoreAway(away + 1);
+    setBalls(ballsInitialState);
+    setStrikes(strikesInitialState);
+  };
+  const addInning = () => setInning(inning + 1);
+  const addBalls = () => {
+    if (balls === "***") {
+      setBalls(ballsInitialState);
+    } else {
+      setBalls(balls + "*");
+    }
+  };
+  const addStrikes = () => {
+    if (strikes === "**") {
+      setStrikes(strikesInitialState);
+      addOuts();
+    } else {
+      setStrikes(strikes + "*");
+    }
+  };
+  const addOuts = () => {
+    if (outs === "**") {
+      setOuts(outsInitialState);
+      setStrikes(strikesInitialState);
+      setBalls(ballsInitialState);
+    } else {
+      setOuts(outs + "*");
+    }
+  };
+  const resetAll = () => {
+    setScoreHome(homeInitialState);
+    setScoreAway(awayInitialState);
+    setBalls(ballsInitialState);
+    setInning(inningInitialState);
+    setStrikes(strikesInitialState);
+    setOuts(outsInitialState);
+  };
+
+  
 
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Yankees</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home_score">{home}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="inn">
+            <h2 className="inning">Inning</h2>
+            <div className="current_inning">{inning}</div>
+          </div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <h2 className="away__name">Guest</h2>
+            <div className="away_score">{away}</div>
           </div>
         </div>
-        <BottomRow />
+        <div className="bottomRow">
+      <div className="balls">
+        <h3 className="balls_title">Balls</h3>
+        <div className="balls_value">{balls}</div>
+      </div>
+      <div className="strikes">
+        <h3 className="strikes_title">Strikes</h3>
+        <div className="strikes_value">{strikes}</div>
+      </div>
+      <div className="outs">
+        <h3 className="outs_title">Outs</h3>
+        <div className="outs_value">{outs}</div>
+      </div> 
+    </div>
       </section>
       <section className="buttons">
         <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__run" onClick={addYankeesRun}>Yankees Score a Run</button>
+          <button className="innings_button" onClick={addInning}>Next Inning</button>
+          <button className="balls_button" onClick={addBalls}>Ball Thrown</button>
+          <button className="strike_button" onClick={addStrikes}>Strike Thrown</button>
+          <button className="out_button" onClick={addOuts}>Current Outs</button>
+          <button className="awayButtons__run" onClick={addGuestRun}>Guest Score a Run</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="reset_button" onClick={() => resetAll()}>Reset All</button>
         </div>
       </section>
     </div>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [balls] = useState(" ");
+  const [strikes] = useState(" ");
+  const [outs] = useState(" ");
+
+  
+
   return (
     <div className="bottomRow">
-      <div className="down">
-        <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+      <div className="balls">
+        <h3 className="balls_title">Balls</h3>
+        <div className="balls_value">{balls}</div>
       </div>
-      <div className="toGo">
-        <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+      <div className="strikes">
+        <h3 className="strikes_title">Strikes</h3>
+        <div className="strikes_value">{strikes}</div>
       </div>
-      <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
-      </div>
-      <div className="quarter">
-        <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+      <div className="outs">
+        <h3 className="outs_title">Outs</h3>
+        <div className="outs_value">{outs}</div>
       </div>
     </div>
   );
